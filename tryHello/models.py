f61a54cebd732d19b62043b87ad960d6ebda391e
Gautam.Sinha <k.gautam711@gmail.com>
2024-10-16 09:39:01 +0530

    How to create superuser

--- tryHello/models.py
+++ tryHello/models.py
@@ -1,3 +1,31 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    dob=models.DateField()
+    is_active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+class Course(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+
+
+class Enrollment(models.Model):
+    student=models.ForeignKey(Student, on_delete=models.CASCADE)
+    course=models.ForeignKey(Course, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+class TestTable(models.Model):
+    name=models.CharField(max_length=100)
+
+    class Meta:
+        db_table='abc_table'
+        ordering=['name']
+
--- tryHello/views.py
+++ tryHello/views.py
@@ -1,7 +1,17 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from tryHello.models import Student
+
+
 # Create your views here.
 def hello_world(request,name):
-    return HttpResponse("Hello" + " "+name)
+    #s1=Student(name=name, age=10, dob=datetime.date.today())
+    #s1.save()
+    student = Student.objects.get(name=name)
+    student.age = 10
+    student.save()
+    return HttpResponse("Hello your age is" + str(student.age))
 
