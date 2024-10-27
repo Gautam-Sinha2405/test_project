4f76c75d679434cf8cf3785427769d916bf9e6b3
Gautam.Sinha <k.gautam711@gmail.com>
2024-10-28 03:01:42 +0530

    BOOK BY ID and return json response & # TODO: GET AUTHOR BY ID and return json response..

--- library/urls.py
+++ library/urls.py
@@ -4,4 +4,6 @@ from library import views
 urlpatterns= [
     path('createbook/', views.create_book, name='createbook'),
     path('createAuthor/', views.create_author, name='createAuthor'),
+    path('get_book_by_id/<int:book_id>', views.get_book_by_id, name='get_book_by_id'),
+    path('get_author_by_id/<int:author_id>', views.get_author_by_id, name='get_author_by_id'),
 ]
--- library/views.py
+++ library/views.py
@@ -6,6 +6,10 @@ from rest_framework.response import Response
 
 from library.serializer import BookSerializer, AuthorSerializer
 
+from library.models import Book
+
+from library.models import Author
+
 
 # Create your views here.
 @api_view(['POST'])
@@ -23,3 +27,25 @@ def create_author(request):
     if author_serializer.is_valid():
         author_serializer.save()
         return Response(author_serializer.data, status=status.HTTP_201_CREATED)
+
+# TODO: GET BOOK BY ID and return json response..
+@api_view(['GET'])
+def get_book_by_id(request,book_id):
+    try:
+        book = Book.objects.get(pk=book_id)
+        book_serializer = BookSerializer(book)
+        return Response(book_serializer.data, status=status.HTTP_200_OK)
+
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+# TODO: GET AUTHOR BY ID and return json response..
+@api_view(['GET'])
+def get_author_by_id(request,author_id):
+    try:
+        author = Author.objects.get(pk=author_id)
+        author_serializer = AuthorSerializer(author)
+        return Response(author_serializer.data, status=status.HTTP_200_OK)
+
+    except Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
